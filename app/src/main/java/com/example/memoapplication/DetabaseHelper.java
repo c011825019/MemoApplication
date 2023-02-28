8f235affb8e2b5a245c29852a67ea961cf643256
c011825019 <c011825019@gmail.com>
2023-02-28 15:05:28 +0900

    Add DetabaseHelper

---
+++ app/src/main/java/com/example/memoapplication/DetabaseHelper.java
@@ -0,0 +1,34 @@
+package com.example.memoapplication;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DetabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "notememo.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public DetabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("CREATE TABLE notememo (");
+        sb.append("_id INTEGER PRIMARY KEY,");
+        sb.append("name TEXT,");
+        sb.append("note TEXT");
+        sb.append(");");
+
+        String sql = sb.toString();
+
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
